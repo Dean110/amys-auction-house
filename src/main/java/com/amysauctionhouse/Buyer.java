f386d5a8eb6de7cce064d13b9a5f8301ab3ea35f
Ben Williams <DeanPBR@gmail.com>
2019-05-19 00:49:13 -0400

    Setting userNames and chacking for email address validation.

--- src/main/java/com/amysauctionhouse/Buyer.java
+++ src/main/java/com/amysauctionhouse/Buyer.java
@@ -1,15 +1,38 @@
 package com.amysauctionhouse;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Buyer {
 
-	public void setName(String string) {
-		// TODO Auto-generated method stub
-		
+	public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+			Pattern.CASE_INSENSITIVE);
+
+	private String name;
+	private String userName;
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getName() {
-		
-		return "Joe Tester";
+		return name;
 	}
-	
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		if (isValidUserName(userName)) {
+			this.userName = userName;
+		} else {
+			this.userName = "";
+		}
+	}
+
+	private boolean isValidUserName(String userName) {
+		Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(userName);
+		return matcher.find();
+	}
 }
--- src/test/java/com/amysauctionhouse/BuyerTest.java
+++ src/test/java/com/amysauctionhouse/BuyerTest.java
@@ -1,22 +1,73 @@
 package com.amysauctionhouse;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BuyerTest {
 
+	Buyer underTest;
+
+	@BeforeEach
+	public void setupForEach() {
+		underTest = new Buyer();
+	}
+
 	@Test
 	@DisplayName("Getter and Setter Test for Buyer Name.")
 	public void buyerShouldHaveAName() {
-		Buyer underTest = new Buyer();
-		underTest.setName("Joe Tester");
+		assertAll("One or more of the names were not set correctly.", 
+			() -> assertNameSetting("Joe Tester"),
+			() -> assertNameSetting("Jane Tester")
+		);
+	}
+
+	@Test
+	@DisplayName("Getter and Setter Test for Buyer UserName.")
+	public void buyerShouldHaveAnUserName() {
+		assertAll("One or more of the userNames were not set correctly.",
+		    () -> assertUserNameIsSet("test@testing.com"),
+		    () -> assertUserNameIsSet("testing@tester.com")
+		);
+	}
+
+	@Test
+	@DisplayName("UserName should be a valid email address.")
+	public void newUserNamesShouldBeValidEmailAddresses() {
+		assertAll(
+			()->assertUserNameIsSet("BOBBY B", ""),
+			()->assertUserNameIsSet("BOBBY B@KL.COM", ""),
+			()->assertUserNameIsSet("Bobby B <bobs@kl.com>", ""),
+			()->assertUserNameIsSet("BOBBY_B@kl.com!", "")
+		);
+	}	
+	
+	private void assertUserNameIsSet(String givenUserName, String expectedUserName) {
+		underTest.setUserName(givenUserName);
 		
-		String buyerName = underTest.getName();
+		String buyerUserName= underTest.getUserName();
 		
-		assertEquals( "Joe Tester", buyerName, "Names should match.");
+		assertEquals(expectedUserName, buyerUserName, "UserNames should match.");
+	}
+
+	private void assertUserNameIsSet(String givenUserName) {
+		underTest.setUserName(givenUserName);
+		
+		String buyerUserName= underTest.getUserName();
+		
+		assertEquals(givenUserName, buyerUserName, "UserNames should match.");
 	}
 	
 	
+	private void assertNameSetting(String givenName) {
+		underTest.setName(givenName);
+
+		String buyerName = underTest.getName();
+
+		assertEquals(givenName, buyerName, "Names should match.");
+	}
+
 }
